85e5a4afeaae5fa2c8402de3ada76398f5ae7244
kyum-q <109158497+kyum-q@users.noreply.github.com>
2022-11-04 14:02:28 +0900

    binding setting

--- app/src/main/java/com/example/android_sns_project/MainActivity.kt
+++ app/src/main/java/com/example/android_sns_project/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.android_sns_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android_sns_project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val binding = ActivityMainBinding.inflate(layoutInflater)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContentView(binding.root)
+        //setContentView(R.layout.activity_main)
     }
 }
