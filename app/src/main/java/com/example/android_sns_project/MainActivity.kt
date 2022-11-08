f8b7aaf3a0e0f28e7bc65ff21677b831e74d8907
kyum-q <109158497+kyum-q@users.noreply.github.com>
2022-11-08 13:01:19 +0900

    fragment

--- app/src/main/java/com/example/android_sns_project/MainActivity.kt
+++ app/src/main/java/com/example/android_sns_project/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.android_sns_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import com.example.android_sns_project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,7 +10,6 @@ class MainActivity : AppCompatActivity() {
     val binding = ActivityMainBinding.inflate(layoutInflater)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(binding.root)
         //setContentView(R.layout.activity_main)
     }
